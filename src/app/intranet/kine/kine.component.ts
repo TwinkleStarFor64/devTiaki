e93b8f59751027bda97339159ba7e2181a5c2017
Xavier64 <xavierpincon@gmail.com>
2023-03-21 12:03:00 +0100

    page accueil kine opto et connexion

--- src/app/intranet/kine/kine.component.ts
+++ src/app/intranet/kine/kine.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./kine.component.scss']
 })
 export class KineComponent {
+  public cards:any = [
+    {
+      title:"Progression",
+      text:" Suivez la progression des vos exercices et les médailles que vous avez gagnez.",
+      button:"Accéder à la page de progression",
+      image:"assets/imgKine/kine1.svg",
+      url:'progression-Kine'
+    },
+    {
+      title:"Programmes",
+      text:" Suivez les programmes prescrit par vôtre kinésithérapeute.",
+      button:"Accéder à la page contenant les programmes",
+      image:"assets/imgKine/kine2.svg",
+      url:'programme-Kine'
+    },
+    {
+      title:"Exercices",
+      text:"Réaliser les exercices en recherche libre ou recommander par vôtre kinésithérapeute.",
+      button:"Accéder à l'espace Kinésithérapie",
+      image:"assets/imgKine/kine3.svg",
+      url:'exercice-Kine'
+    },
 
+  ]
 }
--- src/app/intranet/opto/opto.component.ts
+++ src/app/intranet/opto/opto.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./opto.component.scss']
 })
 export class OptoComponent {
+  public cards:any = [
+    {
+      title:"Progression",
+      text:" Suivez la progression des vos exercices et les médailles que vous avez gagnez.",
+      button:"Accéder à la page de progression",
+      image:"assets/imageOpto/imageOpto1.svg",
+      url:'progression-Kine'
+    },
+    {
+      title:"Programmes",
+      text:" Suivez les programmes prescrit par vôtre optométriste.",
+      button:"Accéder à la page programmes",
+      image:"assets/imageOpto/imageOpto2.svg",
+      url:'programme-Kine'
+    },
+    {
+      title:"Exercices",
+      text:"Réaliser les exercices en recherche libre ou recommander par vôtre optométriste.",
+      button:"Accéder à la page d'exercices",
+      image:"assets/imageOpto/imageOpto3.svg",
+      url:'exercice-Kine'
+    },
 
+  ]
 }
