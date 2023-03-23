760fb7f020d727300d3e655b1b7856df03ef0d29
Xavier64 <xavierpincon@gmail.com>
2023-03-23 11:49:51 +0100

    fin css et contenu des interfaces opto kine creation service exercice-optoet kine

--- src/app/intranet/opto/progression-opto/progression-opto.component.ts
+++ src/app/intranet/opto/progression-opto/progression-opto.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ExerciceI } from 'src/app/intranet/modeles/Types.js';
+import { ExerciceOptoService } from '../exercice-opto/services/exercice-opto.service';
 
 @Component({
   selector: 'app-progression-opto',
@@ -9,79 +10,14 @@ import { ExerciceI } from 'src/app/intranet/modeles/Types.js';
 export class ProgressionOptoComponent implements OnInit {
 
   avatar!:string;
-  public exercices:ExerciceI[] = [
-    {
-      photo:'assets/imageOpto/exerciceOpto1.png',
-      titre:"Muscles profonds",
-      description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra facilisis congue. Duis sit amet leo sed turpis molestie interdum. Praesent feugiat in libero et laoreet.",
-      horloge:'assets/iconeKineOpto/horlogeOpto.svg',
-      materiel:'assets/iconeKineOpto/materielOpto.svg',
-      
-    },
-    {
-      photo:'assets/imageOpto/exerciceOpto2.png',
-      titre:"Étirements",
-      description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra facilisis congue. Duis sit amet leo sed turpis molestie interdum. Praesent feugiat in libero et laoreet.",
-      horloge:'assets/iconeKineOpto/horlogeOpto.svg',
-      materiel:'assets/iconeKineOpto/materielOpto.svg',
-      
-    },
-    {
-      photo:'assets/imageOpto/exerciceOpto3.png',
-      titre:"Grenouille",
-      description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra facilisis congue. Duis sit amet leo sed turpis molestie interdum. Praesent feugiat in libero et laoreet.",
-      horloge:'assets/iconeKineOpto/horlogeOpto.svg',
-      materiel:'assets/iconeKineOpto/materielOpto.svg',
-    },
-    {
-      photo:'assets/imageOpto/exerciceOpto4.png',
-      titre:"Quotidien",
-      description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra facilisis congue. Duis sit amet leo sed turpis molestie interdum. Praesent feugiat in libero et laoreet.",
-      horloge:'assets/iconeKineOpto/horlogeOpto.svg',
-      materiel:'assets/iconeKineOpto/materielOpto.svg',
-     
-    },
-    {
-      photo:'assets/imageOpto/exerciceOpto5.png',
-      titre:"Muscles profonds",
-      description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra facilisis congue. Duis sit amet leo sed turpis molestie interdum. Praesent feugiat in libero et laoreet.",
-      horloge:'assets/iconeKineOpto/horlogeOpto.svg',
-      materiel:'assets/iconeKineOpto/materielOpto.svg',
-      
-    },
-    {
-      photo:'assets/imageOpto/exerciceOpto1.png',
-      titre:"Étirements",
-      description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra facilisis congue. Duis sit amet leo sed turpis molestie interdum. Praesent feugiat in libero et laoreet.",
-      horloge:'assets/iconeKineOpto/horlogeOpto.svg',
-      materiel:'assets/iconeKineOpto/materielOpto.svg',
-      
-    },
-    {
-      photo:'assets/imageOpto/exerciceOpto2.png',
-      titre:"Grenouille",
-      description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra facilisis congue. Duis sit amet leo sed turpis molestie interdum. Praesent feugiat in libero et laoreet.",
-      horloge:'assets/iconeKineOpto/horlogeOpto.svg',
-      materiel:'assets/iconeKineOpto/materielOpto.svg',
-    
-    },
-    {
-      photo:'assets/imageOpto/exerciceOpto3.png',
-      titre:"Quotidien",
-      description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra facilisis congue. Duis sit amet leo sed turpis molestie interdum. Praesent feugiat in libero et laoreet.",
-      horloge:'assets/iconeKineOpto/horlogeOpto.svg',
-      materiel:'assets/iconeKineOpto/materielOpto.svg',
-     
-    }
+  
 
-  ]
-
-  constructor() { }
+  constructor(public exerciceOpto:ExerciceOptoService) { }
 
   ngOnInit(): void {
    
     this.avatar = 'assets/imgAsidebar/cheerleader1.svg';
-  }
- 
+    this.exerciceOpto.getExerciceOpto();
 
+  }
 }
