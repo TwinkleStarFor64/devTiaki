19001fa0e22a24ed287a57fb45462f92b55298a9
Xavier64 <xavierpincon@gmail.com>
2023-06-13 10:15:18 +0200

    responsive

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -3,11 +3,15 @@ import exerciceOpto from './exercice-opto.js';
 import actualites from './actualites.js';
 import nutrition from './nutrition.js';
 import accueil from'./accueil.js';
+import kine from './kine.js';
+import opto from './opto.js';
 
 export const schemaTypes = [
     exercice,
     exerciceOpto,
     actualites,
     nutrition,
-    accueil
+    accueil,
+    kine,
+    opto
 ]
