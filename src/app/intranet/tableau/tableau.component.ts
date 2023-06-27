67d5e1b1acc459235d41a491333f3f0b95500017
Xavier64 <xavierpincon@gmail.com>
2023-06-27 16:33:27 +0200

    scss page profil responsive et tableau de bord(à finir)

--- src/app/intranet/tableau/tableau.component.ts
+++ src/app/intranet/tableau/tableau.component.ts
@@ -7,7 +7,8 @@ import {
   TableauReussiteI,
 } from '../utils/modeles/Types';
 import { TableauService } from './services/tableau.service';
-import {CdkAccordionModule} from '@angular/cdk/accordion';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tableau',
@@ -23,8 +24,90 @@ export class TableauComponent {
   probleme?: TableauBordProblemeI;
   medecin?: TableauBordMedecinI;
   expandedIndex = 0;
+  listeGenerale: string = '';
 
-  public  items = ['Historiques des évenements', 'Médecins', 'Problèmes particuliers', 'Médical', 'Animations/Rencontres', 'La communauté'];
+
+  public items = [
+    { 
+      header: 'Historiques des évenements',
+      content: `
+        <h4>
+          Pour la nutrition
+        </h4>
+        <ul>
+          <li>{{ liste.histoNutri }}</li>
+        </ul>
+        <h4>
+          Sur la kiné
+        </h4>
+        <ul [class.active]>
+          <li>{{ liste.histoKine }}</li>
+          <li>Planche bien soutenue</li>
+        </ul>
+        <h4>
+          La vision et l'optométrie
+        </h4>
+        <ul>
+          <li>{{ liste.histoOpto }}</li>
+          <li>Planche bien soutenue</li>
+        </ul>
+      `
+    },
+    { 
+      header: 'Médecins',
+      content: `
+        <h4>
+          Pour la nutrition
+        </h4>
+        <ul>
+          <li>{{ liste.medecinNutri }}</li>
+        </ul>
+        <h4>
+          Sur la kiné
+        </h4>
+        <ul>
+          <li>{{ liste.general }}</li>
+          <li>Docteur Sebastian</li>
+        </ul>
+        <h4>
+          La vision et l'optométrie
+        </h4>
+        <ul>
+          <li>{{ liste.medecinOpto }}</li>
+          <li>Docteur CHAN</li>
+        </ul>
+      `
+    },
+    { 
+      header: 'Problèmes particuliers',
+      content: `
+        <h4>
+          Pour la nutrition
+        </h4>
+        <ul>
+          <li>{{ liste.problemeNutri }}</li>
+        </ul>
+        <h4>
+          Sur la kiné
+        </h4>
+        <ul>
+          <li>{{ liste.problemeKine }}</li>
+          <li>Docteur Sebastian</li>
+        </ul>
+        <h4>
+          La vision et l'optométrie
+        </h4>
+        <ul>
+          <li>{{ liste.problemeOpto }}</li>
+          <li>Docteur CHAN</li>
+        </ul>
+      `
+    },
+    { header: 'Médical', content: '' },
+    { header: 'Animations/Rencontres', content: '' },
+    { header: 'La communauté', content: '' }
+  ];
+  
 
   public titre: [{}] = [
     {
@@ -37,7 +120,11 @@ export class TableauComponent {
 
   constructor(
     public tableaux: TableauService,
-  ) {}
+    private sanitizer: DomSanitizer
+  ) { }
+sanitizeHTML(html: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(html);
+  }
 
   ngOnInit() {
     this.tableaux.getTableauBordHistorique();
