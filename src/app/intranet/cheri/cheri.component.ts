4cec0ac0fcc1767f68065523d88e8d28bc7f8b0c
exlineo <contact@exlineo.com>
2023-12-22 21:55:55 +0100

    Un peu plus sur la fiche Cheri

--- src/app/intranet/cheri/cheri.component.ts
+++ src/app/intranet/cheri/cheri.component.ts
@@ -3,6 +3,9 @@ import { SharedModule } from '../../partage/shared.module';
 import { InfosService } from 'src/app/partage/services/infos.service';
 import { ActivatedRoute } from '@angular/router';
 import { CheriService } from './cheri.service';
+import { ConnexionService } from 'src/app/partage/services/connexion.service';
+import { CheriI } from '../partage/modeles/Types';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cheri',
@@ -10,17 +13,22 @@ import { CheriService } from './cheri.service';
   styleUrls: ['./cheri.component.scss'],
   standalone:true,
   providers:[CheriService, InfosService],
-  imports: [SharedModule]
+  imports: [CommonModule, SharedModule]
 })
 export class CheriComponent implements OnInit{
 
   get:CheriService = inject(CheriService);
+  conn:ConnexionService = inject(ConnexionService);
   l:InfosService = inject(InfosService);
   route:ActivatedRoute = inject(ActivatedRoute);
+
+  cheri!:CheriI;
 
   id:string | number = -1;
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
+    this.cheri = this.conn.user.cheris[this.id];
+    console.log(this.cheri, this.conn.user);
   }
 }
